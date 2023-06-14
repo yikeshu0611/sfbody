01f4295c9f4d25412708e8e195e9afdd48f50559
zjcn86 <33260177+yikeshu0611@users.noreply.github.com>
2023-06-15 06:53:05 +0800

    Add files via upload

---
+++ R/data_brain.sagittal.R
@@ -0,0 +1,12 @@
+#' @title brain.sagittal
+#'
+#' @description brain.sagittal higher mental functions, motor function areas, motor function area, sensory area, sensory association area, visual area, Oifactory area, emotional area.
+#'
+#' @docType data
+#'
+#' @usage data(brain.sagittal)
+#'
+#' @keywords datasets
+#' @examples
+#' data(brain.sagittal)
+'brain.sagittal'
---
+++ R/data_mandible.R
@@ -0,0 +1,12 @@
+#' @title mandible
+#'
+#' @description mandible outlier data include head, neck, ramus, coronoid and body.
+#'
+#' @docType data
+#'
+#' @usage data(mandible)
+#'
+#' @keywords datasets
+#' @examples
+#' data(mandible)
+'mandible'
